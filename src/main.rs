3f3182b5f04cde67f85cecf02d00a8ebaba38bed
norbertparti <norbertparti07@gmail.com>
2021-12-19 13:22:09 +0100

    TCP Echo Server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{net::TcpListener, io::{AsyncWriteExt, BufReader, AsyncBufReadExt}};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    loop {
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+
+        let (read, mut write) = socket.split();
+
+        let mut reader = BufReader::new(read);
+        let mut line = String::new();
+
+        loop {
+
+
+            let bytes_read = reader.read_line(&mut line).await.unwrap();
+            if bytes_read == 0 {
+                break;
+            }
+
+            write.write_all(&line.as_bytes()).await.unwrap();
+            line.clear();
+
+        }
+    }
+    
 }
